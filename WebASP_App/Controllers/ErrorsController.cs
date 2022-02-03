e68a074bc4a80bedddc24b81cfe85879a8e1f83d
Darina <37955869+dagenator@users.noreply.github.com>
2022-02-03 16:15:12 +0500

    Navigate menu added

---
+++ WebASP_App/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebASP_App.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult Index(string ErrorMessage)
+        {
+            return View(ErrorMessage);
+        }
+    }
+}
